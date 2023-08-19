0cb1e47dbba20483a7330971d2fd9421c7f427f0
Melissa Oliveira <107162134+MelissaOliveiraC@users.noreply.github.com>
2023-08-19 15:17:45 -0300

    Add files via upload

---
+++ simple-interpreter/Program.cs
@@ -0,0 +1,15 @@
+﻿using VerySimpleInterpreter.Lexer;
+using VerySimpleInterpreter;
+using VerySimpleInterpreter.Parser;
+
+var st = new SymbolTable();
+var basicLexer = new BasicLexer("C:\\Users\\melis\\Documents\\GitHub\\Compiladores\\simple-interpreter\\docs\\example.lang", st);
+
+var basicParser = new BasicParser(basicLexer, st);
+
+Console.WriteLine("\n\n[Simple-Interpreter]\n\n>Output\n");
+basicParser.Prog();
+
+Console.WriteLine("\n___________________________________\nSymbol Table: \n\n");
+Console.WriteLine(st);
+Console.WriteLine("___________________________________\n");
